a061d17ea429628a579389b95c3f4ac901277732
Jared Vinson <jvinsonofficial@gmail.com>
2026-05-04 18:50:17 -0400

    Complete Tasks 10-13 for Book Review project

--- final_project/index.js
+++ final_project/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+// Authentication mechanism
 app.use("/customer/auth/*", function auth(req,res,next){
     if(req.session.authorization) {
         let token = req.session.authorization['accessToken'];
@@ -25,3 +26,10 @@ app.use("/customer/auth/*", function auth(req,res,next){
         return res.status(403).json({message: "User not logged in"});
     }
 });
+ 
+const PORT = 5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
